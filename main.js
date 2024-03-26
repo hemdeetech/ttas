ac42d69dacd08b8f5d4c4b0b40788fb4ee7752fe
hemdeetech <140911207+hemdeetech@users.noreply.github.com>
2024-03-26 23:35:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,24 @@ document.getElementById("contactButton").addEventListener("click", function(even
   event.preventDefault(); // Prevent the default link behavior
   document.getElementById("contact").scrollIntoView({ behavior: 'smooth' }); // Scroll to the home section
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const scrollToTopBtn = document.querySelector(".arrow");
+
+  // Show scroll-to-top button when scrolling down
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) { // Adjust this value as needed
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+
+  // Scroll to top function
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
